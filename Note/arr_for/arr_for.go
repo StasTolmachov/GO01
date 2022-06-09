7aaa4f4bb2a4c8df5d4470dfc87df31e8cc08a8b
StasTolmachov <Stas@Tolmachov.dev>
2022-06-09 07:10:53 +0300

    добавил блокнот

---
+++ Note/arr_for/arr_for.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	simplefor(arr)
+}
+
+func simplefor(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+}
---
+++ Note/test/test.go
@@ -0,0 +1,7 @@
+package main
+
+// todo import "fmt"
+
+func main() {
+	fmt.Println()
+}
