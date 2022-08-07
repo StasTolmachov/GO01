0d068a557c9a68976447322758471be955193175
StasTolmachov <stas@tolmachov.dev>
2022-08-07 20:53:10 +0300

    lesson09

--- 08-09/configPkg/myConfigPkg.go
+++ 08-09/configPkg/myConfigPkg.go
@@ -1,48 +1,76 @@
 package MyConfigPkg
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/url"
+	"os"
 )
 
+type DataBase struct {
+	Port         int
+	Db_url       string
+	Jaeger_url   string
+	Sentry_url   string
+	Kafka_broker string
+	Some_app_id  string
+	Some_app_key string
+}
+
 func MyConfigFunc() {
-	fmt.Println("MyConfigFunc")
+	Connect := DataBase{}
 
-	type DataBase struct {
-		port         int
-		db_url       string
-		jaeger_url   string
-		sentry_url   string
-		kafka_broker string
-		some_app_id  string
-		some_app_key string
-	}
-
-	connect := DataBase{}
-	flag.IntVar(&connect.port, "port", 8080, "port")
-	flag.StringVar(&connect.db_url, "db_url", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "db_url")
-	flag.StringVar(&connect.jaeger_url, "jaeger_url", "http://jaeger:16686", "jaeger_url")
-	flag.StringVar(&connect.sentry_url, "sentry_url", "http://sentry:9000", "sentry_url")
-	flag.StringVar(&connect.kafka_broker, "kafka_broker", "kafka:9092", "kafka_broker")
-	flag.StringVar(&connect.some_app_id, "some_app_id", "testid", "some_app_id")
-	flag.StringVar(&connect.some_app_key, "some_app_key", "testkey", "some_app_key")
-
+	flag.IntVar(&Connect.Port, "port", 8080, "port")
+	flag.StringVar(&Connect.Db_url, "db_url", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "db_url")
+	flag.StringVar(&Connect.Jaeger_url, "jaeger_url", "http://jaeger:16686", "jaeger_url")
+	flag.StringVar(&Connect.Sentry_url, "sentry_url", "http://sentry:9000", "sentry_url")
+	flag.StringVar(&Connect.Kafka_broker, "kafka_broker", "kafka:9092", "kafka_broker")
+	flag.StringVar(&Connect.Some_app_id, "some_app_id", "testid", "some_app_id")
+	flag.StringVar(&Connect.Some_app_key, "some_app_key", "testkey", "some_app_key")
 	flag.Parse()
 
-	_, err := url.Parse(connect.db_url)
+	_, err := url.Parse(Connect.Db_url)
 	if err != nil {
 		panic(err)
 	}
-	_, err = url.Parse(connect.jaeger_url)
+	_, err = url.Parse(Connect.Jaeger_url)
 	if err != nil {
 		panic(err)
 	}
-	_, err = url.Parse(connect.sentry_url)
+	_, err = url.Parse(Connect.Sentry_url)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(connect.port, connect.db_url, connect.jaeger_url, connect.sentry_url, connect.kafka_broker, connect.some_app_id, connect.some_app_key)
+	Bufer, err := os.OpenFile("data.json", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		panic(err)
+	}
+	defer Bufer.Close()
+	DataJsonFile := DataBase{}
+
+	err = json.NewDecoder(Bufer).Decode(&DataJsonFile)
+	if err != nil {
+		fmt.Println("Пустой файл, записаны стандартные значения")
+		DataJsonFile = Connect
+		DataJsonFlag, _ := json.Marshal(DataJsonFile)
+		os.WriteFile("data.json", DataJsonFlag, 0777)
+
+	}
+	_, err = url.Parse(DataJsonFile.Db_url)
+	if err != nil {
+		panic(err)
+	}
+	_, err = url.Parse(DataJsonFile.Jaeger_url)
+	if err != nil {
+		panic(err)
+	}
+	_, err = url.Parse(DataJsonFile.Sentry_url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(DataJsonFile)
 
 }
