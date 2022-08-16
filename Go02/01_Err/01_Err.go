4a4ecda40f934bc344d8fe5feac0813db65dd848
StasTolmachov <stas@tolmachov.dev>
2022-08-16 18:15:44 +0300

    1

--- Go02/01_Err/01_Err.go
+++ Go02/01_Err/01_Err.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type MyError struct {
+	code    int
+	message string
+}
+
+func NewMyError(code int, message string) MyError {
+	return MyError{code: code, message: message}
+}
+
+func (e MyError) Error() string {
+	return fmt.Sprintf("%s %d", e.code, e.message)
+}
+
+var ErrorNotFound = NewMyError(404, "Not Found")
+
 func main() {
-	fmt.Println("work")
+	fmt.Println(ErrorNotFound)
 }
