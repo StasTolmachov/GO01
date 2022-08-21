468769628eab8d99c83e5bf2a4903f60064222c9
StasTolmachov <stas@tolmachov.dev>
2022-08-21 18:11:00 +0300

    1

--- Go02/note/note.go
+++ Go02/note/note.go
@@ -1,21 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func culc(a, b int) int {
-	res := a / b
-	return res
+func culc(a, b int) (int, error) {
+	var err error
+	var res int
+	res = a / b
+	return res, err
+}
+
+type ResultStruct struct {
+	Res int
+	Err error
 }
 
 func main() {
 	var a int
 	var b int
-	// var res int
+
+	var result ResultStruct
 
 	a = 9
-	b = 3
+	b = 0
+	
+	result.Res, result.Err = culc(a, b)
 
-	defer fmt.Println(culc(a, b))
+	fmt.Println(result)
+
 	fmt.Println("second")
 
 }
