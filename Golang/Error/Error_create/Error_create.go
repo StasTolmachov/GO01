d4bd8c61c8dd0b11cf351fdb08746ce905b042d4
StasTolmachov <stas@tolmachov.dev>
2022-08-22 03:39:45 +0300

    err done

--- Golang/Error/Error_create/Error_create.go
+++ Golang/Error/Error_create/Error_create.go
@@ -1,42 +1,3 @@
-// package main
-
-// import "fmt"
-
-// type HttpError struct {
-// 	code    int
-// 	message string
-// }
-
-// func (e HttpError) Error() string {
-// 	return fmt.Sprintln(e.code, e.message)
-// }
-
-// func FuncHttpError(code int, message string) HttpError {
-// 	return HttpError{code: code, message: message}
-// }
-
-// var (
-// 	ErrNotFound  = FuncHttpError(404, "not found")
-// 	ErrForbidden = FuncHttpError(403, "forbidden")
-// )
-
-// func GetPage(path string) error {
-// 	switch path {
-// 	case "/passwd":
-// 		return ErrForbidden
-// 	case "/user/1":
-// 		return nil
-// 	default:
-// 		return ErrNotFound
-// 	}
-// }
-
-// func main() {
-// 	fmt.Println(GetPage("/passwd"))
-// 	fmt.Println(GetPage("/user/1"))
-// 	fmt.Println(GetPage("users"))
-// }
-
 package main
 
 import (
@@ -51,52 +12,28 @@ type error interface {
 }
 
 type MyErrorType struct {
-	Err     error
-	Code    int
-	Message string
-	debug   string
+	Err   error
+	debug string
 }
 
 func (e *MyErrorType) Error() string { // interface
-	return fmt.Sprintln(e.Err, e.Code, e.Message, e.debug)
+	return fmt.Sprintln(e.Err, e.debug)
 
 }
 
-func MyErrorFunc(err error, code int, message string) error { // функция для удобства ввода значений в структуру
+// func MyErrorFunc(err error) error { // функция для удобства ввода значений в структуру
 
-	return &MyErrorType{
-		Err:     err,
-		Code:    code,
-		Message: message,
-		debug:   string(debug.Stack()),
-	}
+// 	return &MyErrorType{
+// 		Err:   err,
+// 		debug: string(debug.Stack()),
+// 	}
 
-}
-
-/*
-func dev(a, b int) int {
-
-	defer func() {
-
-		if v := recover(); v != nil {
-			fmt.Println("Функция восстановлена по причине:", v)
-			// var err MyErrorType
-			// err.Err = errors.New("new err")
-			// fmt.Println(MyErrorFunc(&err, 44, "myfunc"))
-
-		}
-
-	}()
-
-	res := a / b
-
-	return res
-}
-*/
+// }
 
 var (
-	temp   = errors.New("Причина остановки функции")
-	errNew MyErrorType
+	temp        = errors.New("Причина остановки функции")
+	errOpenFile MyErrorType
+	errDev      MyErrorType
 )
 
 func OpenFile(path string) error {
@@ -105,18 +42,13 @@ func OpenFile(path string) error {
 
 		if v := recover(); v != nil {
 			fmt.Println("Функция OpenFile была восстановлена")
-			errNew.Err = temp
-			errNew.Err = fmt.Errorf("%w: %v", errNew.Err, v)
-			errNew.debug = string(debug.Stack())
-
+			errOpenFile.Err = temp
+			errOpenFile.Err = fmt.Errorf("%w: %v", errOpenFile.Err, v)
+			errOpenFile.debug = string(debug.Stack())
 		}
-
 	}()
-
 	_, err := os.OpenFile(path, os.O_RDWR, 0777)
-
 	if err != nil {
-		// return err
 		panic("OpenFile - имеет ошибку")
 
 	}
@@ -124,20 +56,30 @@ func OpenFile(path string) error {
 
 }
 
-func main() {
-	// fmt.Println("Задание №1")
-	// fmt.Println(dev(9, 0))
+func Dev(a, b int) int {
+	defer func() {
 
-	// fmt.Println("Задание №2")
+		if v := recover(); v != nil {
+			fmt.Println("Функция Dev была восстановлена")
+			errDev.Err = temp
+			errDev.Err = fmt.Errorf("%w: %v", errDev.Err, v)
+			errDev.debug = string(debug.Stack())
+		}
+	}()
+	res := a / b
+	return res
+}
+
+func main() {
 
 	_ = OpenFile("data.jason")
-	// fmt.Println(err)
-	fmt.Println(errNew)
-	// Bufer, err := os.OpenFile("data.json", os.O_RDWR, 0777)
-	// if err != nil {
 
-	// 	fmt.Println(MyErrorFunc(err, 404, "open file"))
-	// }
+	fmt.Println(errOpenFile)
+
+	fmt.Println("Приложение работает дальше")
+
+	fmt.Println(Dev(9, 0))
+	fmt.Println(errDev)
 
 	fmt.Println("Приложение работает дальше")
 }
